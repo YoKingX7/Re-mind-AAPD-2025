ebaf6a714f450f6a5540ace0c34dafab3b53d5e5
Big_Head_YK <yoking0626@gmail.com>
2025-08-29 13:37:59 +0800

    components-active

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-console.log('Hello world');
+console.log("Hello world");
+
+// 支援頁面上有多組 emotion-group
+document.querySelectorAll(".emotion-group").forEach((group) => {
+    group.addEventListener("click", (e) => {
+        const card = e.target.closest(".emotion-card-tap");
+        if (!card || !group.contains(card)) return;
+
+        // 點到同一張：不動作
+        if (card.classList.contains("is-active")) return;
+
+        // 清掉舊的，設新的
+        group
+            .querySelectorAll(".emotion-card-tap.is-active")
+            .forEach((c) => c.classList.remove("is-active"));
+        card.classList.add("is-active");
+    });
+});
